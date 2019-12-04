6cb93bcfa036670e0acf19d505db131a0731ec7b
christopheml <christopheml@users.noreply.github.com>
2019-12-04 13:03:00 +0100

    [day01] Solution for part 2

--- src/main/kotlin/com/github/christopheml/day01/FuelRequirement.kt
+++ src/main/kotlin/com/github/christopheml/day01/FuelRequirement.kt
@@ -1,17 +1,26 @@
 package com.github.christopheml.day01
 
 import com.github.christopheml.common.PuzzleInput
+import java.lang.Long.max
 
 fun fuelRequirementFor(mass: Long): Long {
     return mass / 3 - 2
 }
 
 fun completeFuelRequirementFor(mass: Long): Long {
-    return fuelRequirementFor(mass)
+    var total = 0L
+    var fuelMass = fuelRequirementFor(mass)
+    while (fuelMass > 0) {
+        total += fuelMass
+        fuelMass = max(fuelRequirementFor(fuelMass), 0L)
+    }
+    return total
 }
 
 fun main() {
     val modules = PuzzleInput(1).asList().map { it.toLong() }
     val totalFuel = modules.map { fuelRequirementFor(it) }.sum()
-    println("The trip will require $totalFuel fuel")
+    val totalFuelAccountingForFuelMass = modules.map { completeFuelRequirementFor(it) }.sum()
+    println("The trip will require $totalFuel fuel, not accounting for fuel mass")
+    println("The trip will require $totalFuelAccountingForFuelMass fuel, accounting for fuel mass")
 }
